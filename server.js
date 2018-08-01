80e559d7cd501eae3f8c146831b6c2fd0aa4fad3
Joaquim Perez <jquim1@hotmail.com>
2018-08-01 21:01:15 +0100

    http/https fixed (??)

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ const HTTP_PORT = 80;
 const HTTPS_PORT = 443;
 
 const fs = require('fs');
-const privateKey  = fs.readFileSync('privateKey.key', 'utf8');
-const certificate = fs.readFileSync('certificate.crt', 'utf8');
-const creds = {key: privateKey, cert: certificate};
+const creds = 
+{
+	key: fs.readFileSync('certs/server-key.pem'),
+  cert: fs.readFileSync('certs/server-crt.pem'), 
+  ca: fs.readFileSync('certs/ca-crt.pem')
+};
 
 //app.set('port', port);
 

fb7b65332396066e20ada2b0603c349918deed13
Joaquim Perez <jquim1@hotmail.com>
2018-08-01 20:52:07 +0100

    http/https fixed (?)

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const app = express();
 const router = require('./router');
 const basicAuth = require('express-basic-auth');
 const db = require('./database/database');
+const cors = require('cors');
 
 db.init();
+app.use(cors());
 app.use(basicAuth({
     users: db.getUsers(),
     challenge: true // <--- needed to actually show the login dialog!
@@ -38,7 +40,6 @@ app.use(function(err, req, res, next) {
 }); 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
--- server.js
+++ server.js
@@ -2,14 +2,19 @@
 
 const app = require('./app');
 const http = require('http');
-const port = 80;
+const https = require('https');
 
-app.set('port', port);
+const HTTP_PORT = 80;
+const HTTPS_PORT = 443;
 
-const server = http.createServer(app);
-server.listen(port,(err) =>
-{
-	if(!err) console.log("started listening");
-});
-server.on('error', (e) => console.log(e));
-server.on('listening', () => console.log("listening"));
+const fs = require('fs');
+const privateKey  = fs.readFileSync('privateKey.key', 'utf8');
+const certificate = fs.readFileSync('certificate.crt', 'utf8');
+const creds = {key: privateKey, cert: certificate};
+
+//app.set('port', port);
+
+const sHttp = http.createServer(app);
+const sHttps = https.createServer(creds,app);
+sHttp.listen(HTTP_PORT);
+sHttps.listen(HTTPS_PORT);

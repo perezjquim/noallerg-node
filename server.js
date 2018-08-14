aee142be98dfb19f360b18079afb00c53f49acc4
Joaquim Perez <jquim1@hotmail.com>
2018-08-14 19:46:17 +0100

    cleanup

--- server.js
+++ server.js
@@ -1,25 +1,3 @@
-#!/usr/bin/env node
-
-const fs = require('fs');
 const app = require('./app');
-const http = require('http');
-const https = require('https');
-const PORT_HTTP = 80;
-const PORT_HTTPS = 443;
-const options = 
-{
-  key: fs.readFileSync('certificates/server.key'),
-  cert: fs.readFileSync('certificates/server.crt')
-};
-
-const server_http = http.createServer(app);
-server_http.listen(process.env.PORT || PORT_HTTP,(err) =>
-{
-	if(!err) console.log("started listening on HTTP");
-});
-
-const server_https = https.createServer(app);
-server_https.listen(process.env.PORT || PORT_HTTPS,(err) =>
-{
-	if(!err) console.log("started listening on HTTPS");
-});
+const port = process.env.PORT || 1234;
+app.listen(port);
